aa5819a3db2cd014e3b038468c1123e93a3ba699
Student <student@BCCSs-MacBook-Pro.local>
2025-01-30 10:05:31 -0600

    completed

---
+++ LiteralMathMethodExercise.js
@@ -0,0 +1,19 @@
+
+ 
+let warmhugs = "Hi, I'm Olaf and I like warm hugs."
+console.log(warmhugs.toUpperCase())
+
+console.log(warmhugs.replace("like" , "love"));
+
+let beenImpaled = "Oh, look at that. I've been impaled."
+console.log(beenImpaled.slice(18))
+
+let dotdotdot ="..."
+console.log(dotdotdot)
+let skullbones ="I don't have a skull...or bones."
+console.log(skullbones)
+console.log(Math.PI);
+
+
+let randomNumber =(Math.random() * 3);
+console.log=(randomNumber)
