02978d4b5adf912e676c6383af8405d95917ed6e
Giannacianci <Gianna.cianci@stu.glasgow.kyschools.us>
2025-01-31 10:12:52 -0600

    Update LiteralMathMethodExercise.js

--- LiteralMathMethodExercise.js
+++ LiteralMathMethodExercise.js
@@ -1,19 +1,19 @@
+//FROZEN EXERCISE
 
- 
 let warmhugs = "Hi, I'm Olaf and I like warm hugs."
-console.log(warmhugs.toUpperCase())
 
+console.log(warmhugs.toUpperCase())
 console.log(warmhugs.replace("like" , "love"));
 
 let beenImpaled = "Oh, look at that. I've been impaled."
 console.log(beenImpaled.slice(18))
 
 let dotdotdot ="..."
-console.log(dotdotdot)
-let skullbones ="I don't have a skull...or bones."
+let skullbones ="I don't have a skull" + dotdotdot + or bones."
 console.log(skullbones)
+
 console.log(Math.PI);
 
-
-let randomNumber =(Math.random() * 3);
-console.log=(randomNumber)
+let randomNumber = Math.random();
+randomNumber *= 3;
+randomNumber = Math.floor(randomNumber);
